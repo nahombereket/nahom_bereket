7034e4feddda949dc1befde5aefb1a123ebd43b4
Nahom Bereket <nahombereket@protonmail.com>
2024-11-04 23:29:52 +0300

    Quartz sync: Nov 4, 2024, 11:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "DM Serif Text",
+        body: "DM Sans",
         code: "JetBrains Mono",
       },
       colors: {
