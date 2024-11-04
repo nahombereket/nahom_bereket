b76c142291e6762f38399847983160f7001a2245
Nahom Bereket <nahombereket@protonmail.com>
2024-11-04 23:11:59 +0300

    Quartz sync: Nov 4, 2024, 11:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🌹 Nahom Bereket",
+    pageTitle: "🪴 Quartz 4.0",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
